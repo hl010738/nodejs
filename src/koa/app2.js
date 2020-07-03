8ce11fab73e8acd1add001c03b0ff07b029a4a45
talent he <talent@cathaypacific.com>
2020-07-03 17:49:52 +0800

    koa洋葱模型 函数调用链

---
+++ src/koa/app2.js
@@ -0,0 +1,30 @@
+// Koa 洋葱模型 函数调用链
+const Koa = require("koa");
+const app = new Koa();
+
+app.use(async function func1(ctx, next) {
+    console.log("func1 start");
+    // 这里会调用func2 并等待返回
+    await next();
+    console.log("func1 end");
+});
+
+app.use(async function func2(ctx, next) {
+    console.log("func2 start");
+    // 这里会调用func3 并等待返回
+    await next();
+    console.log("func2 end");
+});
+
+app.use(async function func3(ctx, next) {
+    console.log("func3 start");
+    await next();
+    console.log("func3 end");
+});
+
+
+app.use(async (ctx) => {
+    ctx.body = "Koa";
+});
+
+app.listen(3000);
