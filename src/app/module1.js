9eba81ddf69c761ccf1bcdf67ffc70a8242aa417
talent he <talent@cathaypacific.com>
2020-07-07 16:06:45 +0800

    导出exports

--- src/app/module1.js
+++ src/app/module1.js
@@ -4,5 +4,7 @@ const myFunction = function (age) {
     return age + 5;
 };
 
+// 自定义名称
+// 这种导出方式适合逐个元素导出
 exports.aaa = info;
 exports.bbb = myFunction;
--- src/app/module2.js
+++ src/app/module2.js
@@ -1,10 +1,17 @@
 const module2 = {
-    info: {name: "zhangsan", age: 20}, func: function (input) {
+    info: {name: "zhangsan", age: 20},
+    func: function (input) {
         return input + 5;
     }
 };
 
+//这两个exports等价
+
+// 这种导出方式可以自定义名字
 //exports.mod2 = module2;
+
+// 这种导出方式更加方便
+// 这种方式适合整个模块导出
 module.exports = module2;
 
 // 导出类
